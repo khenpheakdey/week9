def62d0d8bce76097965f04360663ba759150bfb
Pheakdey Khen <khenpheakdey@gmail.com>
2021-06-04 21:02:40 +0700

    Integrate

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+
+@include('hero')
+@include('client')
+@include('about')
+@include('service')
+@include('portfolio')
+@include('team')
+@include('pricing')
+@include('faqs')
+@include('contact')
+
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,7 @@
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+      <h1 class="logo me-auto"><a href="index.html">Arsha</a></h1>
+        @include('layouts.nav');
+    </div>
+</header>
+
